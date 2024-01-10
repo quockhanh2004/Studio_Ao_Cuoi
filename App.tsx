4b59c3c390467cfa92a86fd54687b3be0f7e6477
khanhpnq <phamngocquockhanh2004@gmail.com>
2024-01-10 22:46:40 +0700

    Tạo projtect

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
